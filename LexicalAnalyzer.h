9a5b290eb320778f2abf2843c48135d750526b64
dariacasey <caseydaria14@yahoo.com>
2024-03-31 14:18:52 -0500

    Segementation Error

---
+++ LexicalAnalyzer.h
@@ -0,0 +1,26 @@
+/* LexicalAnalyzer.h */
+
+#ifndef LEXICALANALYZER_H
+#define LEXICALANALYZER_H
+
+/* Function declarations for the lexical analyzer */
+
+
+int charClass;
+int lexLen;
+int token;
+char nextChar;
+
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
+
+/* Define token codes here if needed */
+
+#endif 
